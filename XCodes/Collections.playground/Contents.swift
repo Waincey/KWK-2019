c9ca1494d8a27a3e05616169df5cf722d4514b95
Apple <apple@Apples-MacBook-Pro-8.local>
2019-08-06 13:18:11 -0400

    Collections

--- XCodes/Collections.playground/Contents.swift
+++ XCodes/Collections.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
+//import UIKit
+//
+//var str = "Hello, playground"
 
-var str = "Hello, playground"
+//                      Array Notes on Paper
+//             Try It: Creating Arrays and Accessing Data
+//var friends = ["Leslie", "Mackenzie", "Lia"]
+//print (friends[1])
+//friends.append("Adriana")
+//friends[1] = "Abby"
+//friends.remove(at: 3)
+//print(friends)
+
+//                     Dictionary Notes on Paper
+//            Try It: Creating and Accessing Dictionaries
+//var birthdays = ["Wynnis": "Jan 4", "Mom": "Nov 1", "Dad": "Sep 1"]
+//print(birthdays)
+
+//                      Practice: Dictionaries
+//var friendsAndFamily = ["Pet1": "Coco", "Pet2": "Mocha", "Pet3": "Klark", "Pet4": "Midnight", "Mom": "Karen", "Dad": "Kin", "Brother": "Wynnis", "BestFriend": "Leslie", "Scout1": "Alli", "Scout2": "Uma"]
+//print(friendsAndFamily)
+//print(friendsAndFamily["Pet1"]!)
+//print(friendsAndFamily["Pet3"]!)
+//print(friendsAndFamily["BestFriend"]!)
+//friendsAndFamily["Scout2"] = nil
+//print(friendsAndFamily)
