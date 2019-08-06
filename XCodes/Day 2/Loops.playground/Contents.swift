a8466b5c27ae0ed2145e4e6ab3d308b8fe31dfc9
Apple <apple@Apples-MacBook-Pro-8.local>
2019-08-06 14:16:55 -0400

    loops

--- XCodes/Day 2/Loops.playground/Contents.swift
+++ XCodes/Day 2/Loops.playground/Contents.swift
@@ -1,3 +1,18 @@
 //import UIKit
 //
 //var str = "Hello, playground"
+
+//                                     Loops Notes on Paper
+//                    Try It: Arrays and Dictionaries
+//    Arrays
+//var friends = ["Leslie", "Lia", "Adriana", "Abby", "Alli"]
+//for friend in friends {
+//    print("Hello, \(friend)!")
+//}
+//
+//    Dictionaries
+//var luckyNum = ["Leslie": "7", "Lia": "19", "Adriana": "6", "Abby": "3", "Alli": "27"]
+//for (friend, number) in luckyNum {
+//    print("\(friend)'s lucky number is \(number)")
+//}
+
