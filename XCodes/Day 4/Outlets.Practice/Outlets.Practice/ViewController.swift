5f5ed4bae80fd61c5fa8b363df435c1e688f1c5a
Waincey <apple@Apples-MacBook-Pro-8.local>
2019-08-08 15:07:04 -0400

    iteration 0

---
+++ XCodes/Day 4/Outlets.Practice/Outlets.Practice/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Outlets.Practice
+//
+//  Created by Apple on 8/8/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtomTapped(_ sender: Any) {
+        if let newTitle = textField.text {
+        appLabel.text = newTitle
+        }
+    
+    }
+
+    
+}
