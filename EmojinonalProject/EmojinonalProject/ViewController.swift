dc0003b65e441015feba8939ec7e6c7f5e39e1d8
Waincey <apple@Apples-MacBook-Pro-8.local>
2019-08-08 16:01:58 -0400

    iteration 2

--- EmojinonalProject/EmojinonalProject/ViewController.swift
+++ EmojinonalProject/EmojinonalProject/ViewController.swift
@@ -9,21 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let emojis = ["☺️" : "Smiling", "😭" : "Cries"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func Smilely(_ sender: Any) {
+    @IBAction func Smilely(_ sender: UIButton!) {
+        let selectedEmotion = sender.titleLabel?.text
        let alertController = UIAlertController(title: "Feeling Splendid?", message: "Don't let anything ruin your awesome day!!!", preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
+
         }
     
-    @IBAction func Crying(_ sender: Any) {
+    @IBAction func Crying(_ sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
         let alertController = UIAlertController(title: "Feeling Sad?", message: "Better things are ahead. Don't stay stuck. Try something new. It may help lift your spirits.", preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
@@ -31,6 +35,29 @@ class ViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+//    @IBAction func Devil(_ sender: Any) {
+//        let alertController = UIAlertController(title: "Feeling Devilish?", message: "Find something to do. Just make sure its safe and without negative consequences.", preferredStyle: UIAlertController.Style.alert)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+//        present(alertController, animated: true, completion: nil)
+//    }
+//
+//    @IBAction func Angel(_ sender: Any) {
+//        let alertController = UIAlertController(title: "Feeling Like an Angel?", message: "Keep up with the fantastic deeds of helpng others!", preferredStyle: UIAlertController.Style.alert)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+//        present(alertController, animated: true, completion: nil)
+//    }
+//
+//    @IBAction func Horror(_ sender: Any) {
+//        let alertController = UIAlertController(title: "Feeling Axious or Scared?", message: "Take some deep breaths. Everything is going to be alright!!!", preferredStyle: UIAlertController.Style.alert)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+//        present(alertController, animated: true, completion: nil)
+//    }
 }
 
 
